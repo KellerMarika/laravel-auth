c40ec4b8aff97509b31c760bace96417160605c9
KellerMarika <marikakeller92@gmail.com>
2023-02-02 09:02:39 +0100

    ho barato

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-     /*    $Admin = [
+    /*    $Admin = [
             'name' => 'Carolina',
             'surname' => 'Durina',
             'birth_date' => '10-10-1993',
@@ -26,20 +26,12 @@ class UsersTableSeeder extends Seeder
         User::factory()->create([
             'name' => 'Carolina',
             'surname' => 'Durina',
-            'birth_date' => '10-10-1993',
-            'email' => "CarolinaDurina93@libero.com",
+            'birth_date' =>  strtotime("2022-06-06"), "\n",
             'password' => '123stella',
-            'is_admin'=>true
+            'is_admin'=>true,
+
+
+         /* non può funzionare */   'email' => "CarolinaDurina93@libero.com",
         ]);
     }
 }
-/* $table->id();
-$table->string('name')->comment('col-6');
-$table->string('surname')->comment('col-6');
-$table->date('birth_date')->comment('col-4');
-$table->string('email')->unique()->comment('col-4');
-$table->string('password')->comment('col-4');
-$table->boolean('is_admin')->default(false);
-$table->timestamp('email_verified_at')->nullable();
-$table->rememberToken();
-$table->timestamps(); */
