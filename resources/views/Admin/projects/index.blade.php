ed86c538c5d89ec17a678b5ceb7169a07056b558
KellerMarika <marikakeller92@gmail.com>
2023-02-03 10:51:38 +0100

    f5 pre faccio casino sicura

--- resources/views/Admin/projects/index.blade.php
+++ resources/views/Admin/projects/index.blade.php
@@ -4,8 +4,18 @@
 
 @section('content')
     <h1>Lista progetti</h1>
-    @dump($projects[0])
+    
     {{-- ciclo su argomento categoria --}}
     {{-- ciclo su projects quelli che corrispondono alla categoria --}}
+
+<div class="projects-container container">
+    <div class="row">
+        <div class="col-8">
+
+        </div>
+    </div>
+</div>
+
+    @dump($projects[0])
     <a href="{{ url('admin/projects/create') }}" class="btn btn-link">crea un nuovo progetto</a>
 @endsection
